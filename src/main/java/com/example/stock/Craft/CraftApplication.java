da3c048d459a8413df229b964bdd759c2ee5b2b0
Mohammed Imran <61236195+mdimran15@users.noreply.github.com>
2022-11-07 10:08:33 +0530

    Add files via upload

---
+++ src/main/java/com/example/stock/Craft/CraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock.Craft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stock/Craft/CraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock.Craft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
